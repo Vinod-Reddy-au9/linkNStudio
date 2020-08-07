28cdaaea18963ccd451f56fcb40b367141a209ed
Vinod-Reddy-au9 <vinbinreddy@gmail.com>
2020-08-07 18:19:18 +0530

    Added sroll bar

--- contact-handler.php
+++ contact-handler.php
@@ -4,26 +4,22 @@
     $visitor_mail = $_POST['email'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
-    $select = $_POST['select'];
-    $image = $_POST['image'];
-
-    $email_from = "kandhukurivinodreddii@gmail.com";
+    $select = $_POST['services'];
+    // $image = $_POST['image'];
     $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_mail.\n".
-                        "User Phone: $phone.\n".
-                            "User Choosen: $select.\n".
-                                "User File: $image.\n".
-                                    "User Message: $message.\n";
-    
+    $email_body = "User Name: ".$name."\n";
+    $email_body .= "User Email: ".$visitor_mail."\n";
+    $email_body .= "User Phone: ".$phone."\n";
+    $email_body .= "User Choosen: ".$select."\n";
+    // $email_body .= "User File: ".$image."\n" ;
+    $email_body .= "User Message: ".$message."\n";
+
+    // $to = "vinbinreddy@gmail.com";
     $to = "vinbinreddy@gmail.com";
 
-    $headers = "Form: $email_from \r\n";
+    // $header = "MIME-Version: 1.0\r\n";
+    // $header .= "Content-type: text/html\r\n";
     
-    $headers = "Reply-T0: $visitor_mail \r\n";
-    
-    mail($to, $email_subject, $email_body, $headers);
-
-    header("Location: index.html");
+    mail($to, $email_subject, $email_body);
 
 ?>
